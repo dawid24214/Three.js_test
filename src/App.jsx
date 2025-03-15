b48870b784e58ef7c6daec68e283c48c03071f44
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-15 18:49:53 +0100

    cube_3D

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
+import * as THREE from "three";
 
 
-function App() {
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
-  return (
-    <div>
 
-    </div>
-  )
+const renderer = new  THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+function animate() {
+
+ requestAnimationFrame(animate);
+ cube.rotation.x += 0.01;
+ cube.rotation.y += 0.01;
+ renderer.render(scene, camera);
 }
 
-export default App
+export default animate
